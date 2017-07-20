834cc1877e4dec07c0563b82cc15532cd3012de2
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-20 16:06:30 +0100

    Change all element words to item

--- spec/ListSpec.js
+++ spec/ListSpec.js
@@ -12,34 +12,34 @@ describe("List", function() {
     });
   });
 
-  describe('Add element to List', function() {
+  describe('Add item to List', function() {
     it("Should return new list", function() {
-      list.addElementToList("Apples", "1.00", "Imported from Africa")
+      list.addItemToList("Apples", "1.00", "Imported from Africa")
       expect(list.viewList()).toEqual([[1, "Apples", "1.00", "Imported from Africa"]]);
     });
   });
 
-  describe('Delete element from List', function() {
+  describe('Delete item from List', function() {
     it("Should return new list", function() {
-      list.addElementToList("Apples", "1.00", "Imported from Africa")
-      list.addElementToList("Banana", "2.00", "Imported from EU")
-      list.removeElementFromList(1)
+      list.addItemToList("Apples", "1.00", "Imported from Africa")
+      list.addItemToList("Banana", "2.00", "Imported from EU")
+      list.removeItemFromList(1)
       expect(list.viewList()).toEqual([[2, "Banana", "2.00", "Imported from EU"]]);
     });
   });
 
-  describe('Edit element in List', function() {
+  describe('Edit item in List', function() {
     it("Should return new list", function() {
-      list.addElementToList("Apples", "1.00", "Imported from Africa")
-      list.editElementInList(1, "Apples", "1.50", "Imported from Africa")
+      list.addItemToList("Apples", "1.00", "Imported from Africa")
+      list.editItemInList(1, "Apples", "1.50", "Imported from Africa")
       expect(list.viewList()).toEqual([[1, "Apples", "1.50", "Imported from Africa"]]);
     });
   });
 
   describe('Display List', function() {
     it("Should return list", function() {
-      list.addElementToList("Apples", "1.00", "Imported from Africa")
-      list.addElementToList("Banana", "2.00", "Imported from EU")
+      list.addItemToList("Apples", "1.00", "Imported from Africa")
+      list.addItemToList("Banana", "2.00", "Imported from EU")
       expect(list.viewList()).toEqual([
           [1, "Apples", "1.00", "Imported from Africa"],
           [2, "Banana", "2.00", "Imported from EU"]
