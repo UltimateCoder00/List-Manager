10be287768a200543872ae89442360158003ed19
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-20 16:09:24 +0100

    Add Display a single item in list test

--- spec/ListSpec.js
+++ spec/ListSpec.js
@@ -12,14 +12,14 @@ describe("List", function() {
     });
   });
 
-  describe('Add item to List', function() {
+  describe('Add item to list', function() {
     it("Should return new list", function() {
       list.addItemToList("Apples", "1.00", "Imported from Africa")
       expect(list.viewList()).toEqual([[1, "Apples", "1.00", "Imported from Africa"]]);
     });
   });
 
-  describe('Delete item from List', function() {
+  describe('Delete item from list', function() {
     it("Should return new list", function() {
       list.addItemToList("Apples", "1.00", "Imported from Africa")
       list.addItemToList("Banana", "2.00", "Imported from EU")
@@ -28,7 +28,7 @@ describe("List", function() {
     });
   });
 
-  describe('Edit item in List', function() {
+  describe('Edit item in list', function() {
     it("Should return new list", function() {
       list.addItemToList("Apples", "1.00", "Imported from Africa")
       list.editItemInList(1, "Apples", "1.50", "Imported from Africa")
@@ -36,7 +36,7 @@ describe("List", function() {
     });
   });
 
-  describe('Display List', function() {
+  describe('Display list', function() {
     it("Should return list", function() {
       list.addItemToList("Apples", "1.00", "Imported from Africa")
       list.addItemToList("Banana", "2.00", "Imported from EU")
@@ -44,6 +44,16 @@ describe("List", function() {
           [1, "Apples", "1.00", "Imported from Africa"],
           [2, "Banana", "2.00", "Imported from EU"]
         ]);
+    });
+  });
+
+  describe('Display a single item in list', function() {
+    it("Should return list", function() {
+      list.addItemToList("Apples", "1.00", "Imported from Africa")
+      list.addItemToList("Banana", "2.00", "Imported from EU")
+      expect(list.viewSingleItem(2)).toEqual(
+          [2, "Banana", "2.00", "Imported from EU"]
+        );
     });
   });
 });
