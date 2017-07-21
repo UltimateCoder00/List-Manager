21d2f5e0fbdd7ec86a3103c036366e79314f59f1
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-21 11:03:54 +0100

    Add getJson call

--- js/List.js
+++ js/List.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-  $(function() { alert('hi') })
+  var list = new List();
+  list.addItemToList("Apples", "1.00", "Imported from Africa")
+
+  $(function(){
+    $.getJSON('/products', function(data) {
+        console.log(data);
+        alert(list.viewList())
+    });
+  });
+
+  // $(function() {
+  //   $("p").text(list.viewList());
+  //
+  // });
 });
