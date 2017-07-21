995561eb0733b88c54fe2030874397ecc8d573bf
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-21 13:18:27 +0100

    Display list prices

--- js/List.js
+++ js/List.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
-  var list = new List();
-  list.addItemToList("Apples", "1.00", "Imported from Africa")
 
-  $(function(){
-    $.getJSON('/products', function(data) {
-        console.log(data);
-        alert(list.viewList())
+  $.getJSON( "/products", function( data ) {
+    console.log(data[0]);
+    var items = [];
+    $.each( data, function( i ) {
+      items.push( "<li id='" + i + "'>" + data[i].PRICE + "</li>" );
     });
-  });
 
-  // $(function() {
-  //   $("p").text(list.viewList());
-  //
-  // });
+    $( "<ul/>", {
+      "class": "my-new-list",
+      html: items.join( "" )
+    }).appendTo( "body" );
+  });
 });
