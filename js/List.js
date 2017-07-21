0ff2d62b4da5a971a48a1b730349245996780d42
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-21 14:08:21 +0100

    Add remove display table function

--- js/List.js
+++ js/List.js
@@ -1,30 +1,39 @@
 $(document).ready(function() {
+  showList();
+  removeShowList();
+  showList();
 
-  $.getJSON( "/products", function( data ) {
-    console.log(data[0]);
-    var items = [];
+  function showList() {
+    $.getJSON( "/products", function( data ) {
+      console.log(data[0]);
+      var items = [];
 
-    items.push( "<table>" );
+      items.push( "<table id='list-table'>" );
 
-    items.push( "<tr>" );
-    items.push( "<th>ID</th>" );
-    items.push( "<th>Name</th>" );
-    items.push( "<th>Price</th>" );
-    items.push( "</tr>" );
-
-    $.each( data, function( i ) {
       items.push( "<tr>" );
-      items.push( "<th>" + data[i].ID + "</th>" );
-      items.push( "<th>" + data[i].NAME + "</th>" );
-      items.push( "<th>£" + data[i].PRICE + "</th>" );
+      items.push( "<th>ID</th>" );
+      items.push( "<th>Name</th>" );
+      items.push( "<th>Price</th>" );
       items.push( "</tr>" );
+
+      $.each( data, function( i ) {
+        items.push( "<tr>" );
+        items.push( "<th>" + data[i].ID + "</th>" );
+        items.push( "<th>" + data[i].NAME + "</th>" );
+        items.push( "<th>£" + data[i].PRICE + "</th>" );
+        items.push( "</tr>" );
+      });
+
+      items.push( "</table>" );
+
+      $( "<ul/>", {
+        "class": "my-new-list",
+        html: items.join( "" )
+      }).appendTo( "body" );
     });
+  }
 
-    items.push( "</table>" );
-
-    $( "<ul/>", {
-      "class": "my-new-list",
-      html: items.join( "" )
-    }).appendTo( "body" );
-  });
+  function removeShowList() {
+    $.closest('table#list-table').remove();
+  }
 });
