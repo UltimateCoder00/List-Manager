c6b0d79e2f309ec7ed74fc32e5bbd9c836c273b9
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-07-21 13:26:01 +0100

    Display full table

--- js/List.js
+++ js/List.js
@@ -3,9 +3,24 @@ $(document).ready(function() {
   $.getJSON( "/products", function( data ) {
     console.log(data[0]);
     var items = [];
+
+    items.push( "<table>" );
+
+    items.push( "<tr>" );
+    items.push( "<th>ID</th>" );
+    items.push( "<th>Name</th>" );
+    items.push( "<th>Price</th>" );
+    items.push( "</tr>" );
+
     $.each( data, function( i ) {
-      items.push( "<li id='" + i + "'>" + data[i].PRICE + "</li>" );
+      items.push( "<tr>" );
+      items.push( "<th>" + data[i].ID + "</th>" );
+      items.push( "<th>" + data[i].NAME + "</th>" );
+      items.push( "<th>£" + data[i].PRICE + "</th>" );
+      items.push( "</tr>" );
     });
+
+    items.push( "</table>" );
 
     $( "<ul/>", {
       "class": "my-new-list",
